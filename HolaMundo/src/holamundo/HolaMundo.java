3a5bff881f8ee561451c07e64ccd1b84203bb86a
Rafael Diaz Handal <rafadiazh98@gmail.com>
2017-04-25 22:38:51 -0600

    Terminee

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -15,7 +15,7 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("PRUEBA PARA VER SI FUNCIONA");
     }
     
 }
